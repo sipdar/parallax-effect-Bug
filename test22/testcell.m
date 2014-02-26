4a2269aab10db1f86f417dd0d4f80f9dc6415182
Sunny <sipdar@163.com>
2014-02-26 18:12:37 +0800

    fix bug

--- test22/testcell.m
+++ test22/testcell.m
@@ -21,20 +21,21 @@
 
 -(void)awakeFromNib {
 	[super awakeFromNib];
+
 }
 -(void)addCoustomView{
-	for (UIView *view in [self.contentView subviews]) {
-		if (view.frame.size.width ==100) {
-			[view removeFromSuperview];
-		}
-	}
 	
 	[self.coustomView removeFromSuperview];
-	UIView *view  = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
-	view.backgroundColor = [UIColor blackColor];
-	view.center = CGPointMake(self.contentView.center.x, self.contentView.center.y -50);
-	[self.contentView addSubview:view];
-	self.coustomView = view;
+	self.coustomView.hidden = YES;
+	if (self.coustomView == nil) {
+		UIView *view  = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+		view.backgroundColor = [UIColor blackColor];
+		view.center = CGPointMake(self.contentView.center.x, self.contentView.center.y -50);
+		[self.contentView addSubview:view];
+		self.coustomView = view;
+	}
+	[self.contentView addSubview:self.coustomView];
+	self.coustomView.hidden = NO;
 }
 /*
 // Only override drawRect: if you perform custom drawing.
